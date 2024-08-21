3c72099913c6c83402b493b7f7274f7665c4139c
shengcen <51124420+shengcen@users.noreply.github.com>
2024-08-21 13:08:56 +0800

    Update LM.py

--- data/my_dataset/LM.py
+++ data/my_dataset/LM.py
@@ -7,7 +7,6 @@ import networkx as nx
 # initial_LP = gp.Model('initial LP')  # 定义变量initial_LP，调用Gurobi的Model，选择Initial Programming（整数规划）模型
 # x = {}  # 创建一个空字典来存储决策变量
 
-
 fiber_cost = []
 ip_cost = []
 
@@ -16,23 +15,65 @@ ip_cost = []
 # link_num = 300
 
 
+## > 1 day unsplittable
+# flow_num = 60
+# node_num = 20
+# link_num = 150
+
+## > 1 day splittable
+# flow_num = 60
+# node_num = 20
+# link_num = 150
+
+
+# flow_num = 50
+# node_num = 20
+# link_num = 100
+
+
+# ## 139500
+# flow_num = 20
+# node_num = 10
+# link_num = 50
+
+## 139500
+# flow_num = 40
+# node_num = 30
+# link_num = 200
+
+
+
+
+
+
+# # opt:197000
+# flow_num = 20
+# node_num = 20
+# link_num = 80
+
+# # opt:298500
 # flow_num = 30
 # node_num = 20
 # link_num = 80
 
+# ## opt: 99500
 # flow_num = 6
 # node_num = 7
 # link_num = 20
 
+
 ## opt: 87500
-flow_num = 16
+flow_num = 13
 node_num = 7
-link_num = 20
+## is even
+link_num = 30
 
-# SEED_A = 3
-SEED_A = 2
 
-random.seed(SEED_A)
+# ## opt: 87500
+# flow_num = 30
+# node_num = 15
+# link_num = 100
+
 
 dmd_max = 2000
 
@@ -41,6 +82,18 @@ dmd_max = 2000
 mini_unit = 500
 max_fiber = 3
 capacity_per_fiber = 1000
+
+
+# SEED_A = 3
+SEED_A = 4
+
+
+
+
+
+
+# fiber_cost = 100
+random.seed(SEED_A)
 
 from_list = []
 to_list = []
@@ -53,11 +106,14 @@ while i < link_num:
     my_to = random.randint(0, node_num-1)
     if my_to == my_from:
         my_to = (my_to + 1) % node_num
-    if [my_from, my_to] not in overall_list:
+    if ([my_from, my_to] not in overall_list) and ([my_to, my_from] not in overall_list):
         overall_list.append([my_from,my_to])
+        overall_list.append([my_to, my_from])
         from_list.append(my_from)
         to_list.append(my_to)
-        i += 1
+        from_list.append(my_to)
+        to_list.append(my_from)
+        i += 2
 # exit(0)
 
 # for i in range(node_num):
@@ -73,12 +129,14 @@ while i < link_num:
 
 
 ## fiber cost 在那个范围之间分布
-for i in range(len(from_list)):
+for i in range(link_num//2):
     # random.seed(time.time())
     # fiber_cost.append(random.randint(100,200))
-    fiber_cost.append(0)
-    # ip_cost.append(random.randint(1,10))
-    ip_cost.append(1)
+    # fiber_cost.append(0)
+    ip_cost.append(random.randint(1,10))
+    ## 双向ip cost相等
+    # ip_cost.append(1)
+
 
 
 
@@ -101,8 +159,8 @@ for i in range(flow_num):
     dst_list.append(aa)
     # random.seed(time.time())
     ## dmd 在哪些地方 浮动
-    # dmd_list.append(random.randint(100, 2000))
-    dmd_list.append(1000)
+    dmd_list.append(random.randint(100, 2000))
+    # dmd_list.append(1000)
 
     # print(src_list[i])
     # print(dst_list[i])
@@ -121,13 +179,7 @@ my_link = {"from":from_list, "to":to_list}
 my_node = list(range(node_num))
 
 ## abandoned
-# dmd_max = 6000
-#
-# cap_max = 3000
-# # mini_unit = 100
-# mini_unit = 500
-# max_fiber = 3
-# capacity_per_fiber = 1000
+
 
 out_dict = {}
 
@@ -149,155 +201,128 @@ for i in range(len(my_node)):
             in_link_idx.append(link_idx)
     in_dict[str(i)] = in_link_idx
 
+## new
+my_path = []
+Gbase = nx.DiGraph()
+Gbase.add_nodes_from(my_node)
+edge_list = []
+for i in range(len(from_list)):
+    # edge_list.append((from_list[i],to_list[i]))
+    Gbase.add_weighted_edges_from([(from_list[i],to_list[i],{'weight':ip_cost[i//2]})])
+
+def get_k_shortest_paths(graph, source, target, k):
+    paths = nx.shortest_simple_paths(graph, source, target)
+    shortest_paths = []
+    for path in paths:
+        shortest_paths.append(path)
+        if len(shortest_paths) == k:
+            break
+    return shortest_paths
 
 
-
-
-# my_flow = {"src":[2,4,3,1,18,17,3,2,16,17,18,16], "dst":[3,0,1,2,3,4,2,18,17,18,3,3], "dmd": [200,300,400,2000,200,1000.5,1200.5,1800,400,1500,2000,100]}
-# # 双向的link
-# my_link = {"from":[0,0,0,0,1,1,1,1,2,2,2,3,3,3,3,4,4,4,4,2,18,17,3,17,18,16,18,16], "to":[1,2,3,4,0,2,3,4,0,1,4,0,1,2,4,0,1,2,3,3,2,0,18,18,17,17,16,2], "cap": [2000 for i in range(28)]}
-# my_node = list(range(20))
-# dmd_max = 6000
-# cap_max = 2000
-
-# out_dict = {}
+# my_path: [[[a,b],[b,c],]
+for i in range(flow_num):
+    my_path.append([])
+    curr_flow = i
+    k_shortest_paths = get_k_shortest_paths(Gbase, source=my_flow["src"][curr_flow], target=my_flow["dst"][curr_flow],k=4)
+    for i, path in enumerate(k_shortest_paths):
+        my_path[-1].append(path)
 #
-# for i in range(len(my_node)):
-#     out_dict[str(i)] = []
-#     out_link_idx = []
-#     for link_idx in range(len(my_link["from"])):
-#         if my_link["from"][link_idx] == i:
-#             out_link_idx.append(link_idx)
-#     out_dict[str(i)] = out_link_idx
-#
-# in_dict = {}
-#
-# for i in range(len(my_node)):
-#     in_dict[str(i)] = []
-#     in_link_idx = []
-#     for link_idx in range(len(my_link["to"])):
-#         if my_link["to"][link_idx] == i:
-#             in_link_idx.append(link_idx)
-#     in_dict[str(i)] = in_link_idx
-
-
-
+#     print(my_path[-1])
+# exit(0)
 
 
 
 # print(my_flow["dst"])
 
 
-
 # dmd_max:flow最大的demand情况
 
-def my_solver_LP(my_flow, my_link, my_node,link_num,flow_num,dmd_max,allocated_capacity):
+def my_solver_LP(ip_cost,my_flow, my_link, my_node,link_num,flow_num,dmd_max,my_path,allocated_capacity):
     # flow_cnt = len(my_flow["src"])
     # link_cnt = len(my_link["from"])
     # node_cnt = len(my_node)
 
     initial_LP = gp.Model('initial LP')
 
-    out_dict = {}
+    flow_cnt = len(my_flow["src"])
+    link_cnt = len(my_link["from"])
+    node_cnt = len(my_node)
 
-    for i in range(len(my_node)):
-        out_dict[str(i)] = []
-        out_link_idx = []
-        for link_idx in range(len(my_link["from"])):
-            if my_link["from"][link_idx] == i:
-                out_link_idx.append(link_idx)
-        out_dict[str(i)] = out_link_idx
-
-    in_dict = {}
-
-    for i in range(len(my_node)):
-        in_dict[str(i)] = []
-        in_link_idx = []
-        for link_idx in range(len(my_link["to"])):
-            if my_link["to"][link_idx] == i:
-                in_link_idx.append(link_idx)
-        in_dict[str(i)] = in_link_idx
-
-    x = [[0 for j in range(flow_num)] for i in range(link_num)]
+    # x = [[0 for j in range(flow_cnt)] for i in range(link_cnt)]
+    # x[i][j]: allocated data rate of i^th flow's j^th path
+    x = [[0 for j in range(4)] for i in range(flow_cnt)]
+    f = [[0 for j in range(link_cnt)] for i in range(flow_cnt)]
 
     # for i in range(link_cnt*flow_cnt):  # 创建两个决策变量
     #     # 下界lb为0，上界ub为正无穷，变量类型vtype为连续型，变量名称name为x0和x1
     #     x[i] = initial_LP.addVar(lb=0, ub=dmd_max, vtype=GRB.CONTINUOUS, name='x_' + str(i))
 
-    for i in range(link_num):
-        for j in range(flow_num):
-            # 下界lb为0，上界ub为正无穷，变量类型vtype为连续型，变量名称name为x0和x1
-            x[i][j] = initial_LP.addVar(lb=0, ub=dmd_max, vtype=gp.GRB.CONTINUOUS, name=str(i)+"th link and "+str(j)+"th flow")
-            # x[i][j] = initial_LP.addVar(lb=0, ub=dmd_max, vtype=GRB.INTEGER,
-            #                             name=str(i) + "th link and " + str(j) + "th flow")
 
+    for i in range(flow_cnt):
+        # 下界lb为0，上界ub为正无穷，变量类型vtype为连续型，变量名称name为x0和x1
+        for j in range(link_cnt):
+            f[i][j] = initial_LP.addVar(lb=0, ub=dmd_max, vtype=gp.GRB.CONTINUOUS,
+                                        name=str(i) + "th flow and " + str(j) + "th link")
 
-    # initial_LP.setObjective(100 * x[0] + 150 * x[1], GRB.MINIMIZE)  # 目标函数，设置为最大化MAXIMIZE
-    # initial_LP.setObjective(
-    #     gp.quicksum(ip_cost[i] * cap[i] * mini_unit  for i in range(link_num)), gp.GRB.MINIMIZE)
-    initial_LP.setObjective(np.sum(x), gp.GRB.MINIMIZE)  # 目标函数，设置为最大化MAXIMIZE
-    # initial_LP.setObjective(gp.quicksum(ip_cost[i] * x[i] for i in range(link_num)), gp.GRB.MINIMIZE)
+        for j in range(4):
+            x[i][j] = initial_LP.addVar(lb=0, ub=dmd_max, vtype=gp.GRB.CONTINUOUS,
+                                        name=str(i) + "th flow and " + str(j) + "th split")
 
+    for i in range(flow_cnt):
+        for j in range(4):
+            if j >= len(my_path[i]):
+                initial_LP.addConstr(x[i][j] == 0)
 
+        initial_LP.addConstr(np.sum(x[i]) == my_flow["dmd"][i])
 
+    initial_LP.setObjective(
+        gp.quicksum(ip_cost[i] for i in range(link_cnt // 2)), gp.GRB.MINIMIZE)
 
+    ## calculate f[i][j]: the allocated data rate for i^th flow on j^th link
+    for i in range(flow_cnt):
+        # by link index
+        split1_link_list = []
+        split2_link_list = []
+        split3_link_list = []
+        split4_link_list = []
+        for m in range(link_cnt):
+            link_set = []
+            this_from = my_link["from"][m]
+            this_to = my_link["to"][m]
+            split_cnt = 0
+            for split in range(len(my_path[i])):
+                split_cnt += 1
+                for imm in range(len(my_path[i][split]) - 1):
+                    if this_from == my_path[i][split][imm] and this_to == my_path[i][split][imm + 1]:
+                        if split_cnt == 1:
+                            split1_link_list.append(m)
+                            link_set.append(0)
+                        elif split_cnt == 2:
+                            split2_link_list.append(m)
+                            link_set.append(1)
+                        elif split_cnt == 3:
+                            split3_link_list.append(m)
+                            link_set.append(2)
+                        elif split_cnt == 4:
+                            split4_link_list.append(m)
+                            link_set.append(3)
+            initial_LP.addConstr(gp.quicksum(x[i][t] for t in link_set) == f[i][m])
 
-    # initial_LP.addConstr(2 * x[0] + x[1] <= 10)  # 约束条件1
-    # initial_LP.addConstr(3 * x[0] + 6 * x[1] <= 40)  # 约束条件2
-    # initial_LP.addConstr(2 * x[0][0] + x[1][0] >= 10)
-    for i in range(link_num):
+    for i in range(link_cnt // 2):
         # for j in range(flow_cnt):
-        #     initial_LP.addConstr(x[i][j] <= my_link["cap"][i])
-        # initial_LP.addConstr(np.sum(x[i]) <= my_link["cap"][i])
-        initial_LP.addConstr(np.sum(x[i]) <= allocated_capacity[i])
+        # initial_LP.addConstr(x[i][j] <= cap[i])
 
-    for j in range(flow_num):
-        curr_src = my_flow["src"][j]
-        curr_dst = my_flow["dst"][j]
-        curr_dmd = my_flow["dmd"][j]
+        initial_LP.addConstr(gp.quicksum(f[s][2 * i] + f[s][2 * i + 1] for s in range(flow_cnt)) <= allocated_capacity[i])
+        # initial_LP.addConstr(cap[i] * mini_unit  <= U[i] * capacity_per_fiber)
 
-        a = 0
-        b = 0
-        if len(out_dict[str(curr_src)]) > 0:
-            for s in out_dict[str(curr_src)]:
-                a += x[s][j]
-        if len(in_dict[str(curr_src)]) > 0:
-            for s in in_dict[str(curr_src)]:
-                b += x[s][j]
+    # for i in range(link_cnt // 2):
+    #     # if link_cnt % 2 == 0:
+    #     #     initial_LP.addConstr(cap[i] * mini_unit + cap[(i + 1) % link_cnt] * mini_unit <= U[i] * capacity_per_fiber)
+    #     # initial_LP.addConstr(cap[i] * mini_unit  <= U[i] * capacity_per_fiber)
+    #     initial_LP.addConstr(cap[i] * mini_unit <= max_fiber * capacity_per_fiber)
 
-
-        initial_LP.addConstr(a-b==my_flow["dmd"][j])
-
-        a = 0
-        b = 0
-        if len(out_dict[str(curr_dst)]) > 0:
-            for s in out_dict[str(curr_dst)]:
-                a += x[s][j]
-        if len(in_dict[str(curr_dst)]) > 0:
-            for s in in_dict[str(curr_dst)]:
-                b += x[s][j]
-
-        initial_LP.addConstr(b - a == my_flow["dmd"][j])
-
-        for other_node in my_node:
-            if other_node is not curr_src and other_node is not curr_dst:
-                a = 0
-                b = 0
-                if len(out_dict[str(other_node)]) > 0:
-                    for s in out_dict[str(other_node)]:
-                        a += x[s][j]
-                if len(in_dict[str(other_node)]) > 0:
-                    for s in in_dict[str(other_node)]:
-                        b += x[s][j]
-
-                initial_LP.addConstr(a - b == 0)
-    # try:
-    #     initial_LP.optimize()  # 调用求解器
-    #     return True
-    # except:
-    #     return False
-
-    initial_LP.optimize()
+    initial_LP.optimize()  # 调用求解器
 
     try:
         print(initial_LP.ObjVal)
@@ -313,7 +338,7 @@ def my_solver_LP(my_flow, my_link, my_node,link_num,flow_num,dmd_max,allocated_c
 import time
 s1 = time.time()
 
-print(my_solver_LP(my_flow, my_link, my_node,link_num,flow_num,dmd_max=dmd_max,allocated_capacity=[max_fiber*capacity_per_fiber for i in range(link_num)]))
+print(my_solver_LP(ip_cost,my_flow, my_link, my_node,link_num,flow_num,dmd_max=dmd_max,my_path=my_path,allocated_capacity=[0 for i in range(link_num//2)]))
 
 s2 = time.time()
 
